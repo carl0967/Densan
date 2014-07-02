8142433d2146951bb0cd22f794d1139359885bcc
carl0967 <yblueycarlo1967@gmail.com>
2014-07-02 11:34:37 +0900

    AObject.hの更新
    エラー訂正
    クラスファイルの最後の ;　忘れ
    x,y,およびspeedをintからdoubleに修正

--- AObject.h
+++ AObject.h
@@ -1,25 +1,29 @@
 #pragma once
 
+/*
+「物体」の抽象クラス
+*/
+
 class AObject{
 public:
-	AObject(double x,double y);  
+	AObject(double ax,double ay);  
 	void Move();                  //自身のスピード分移動する
 	void Draw(int offset);   //offsetを使って自身の描画位置を算出して描画する
-	virtual void Think()=0;  //自身のスピードを操作して行動を決める
+	 void Think();  //自身のスピードを操作して行動を決める
 	void TouchedWall();   //壁と当たったときに呼ばれる
 	void Fall(double g);  // 引数の重力分落ちる
 
 protected:
 	int made_time;   //生成された時間（Fieldからみた）
 	static int graphic_handle;  //どのメモリに画像が読み込まれているか
-	int x;   //x座標
-	int y;  //y座標
-	int alive; //生存していればtrue
-	int speed_x; //x軸のスピード
-	int speed_y; //y軸のスピード
+	double x;   //x座標
+	double y;  //y座標
+	bool alive; //生存していればtrue
+	double speed_x; //x軸のスピード
+	double speed_y; //y軸のスピード
 	int width; //オブジェクトの幅 (当たり判定用)
 	int height; //オブジェクトの高さ
 	bool right; //向きを表す。右向きならtrue。左向きならfalse
 	bool aerial; //空中にいるならtrue。地面に接しているならfalse
 
-}
+};
