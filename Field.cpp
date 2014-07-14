ba73ada73ed87515eed3f182ea9d029f295e3348
222yuta <scrap2722@yahoo.co.jp>
2014-07-14 18:33:36 +0900

    Fieldの更新
    追加した関数
    ・count_のゲッター
    ・offsetを計算する関数 CalcOffset

--- Field.cpp
+++ Field.cpp
@@ -1,14 +1,10 @@
 #include "Field.h"
-#include "Map.h"
-#include "AObject.h"
-#include <vector>
 
 
-
-Field::Field(void)
-{
+Field::Field(Map map){
+	map_ = map;
+	gravity_ = 1;
 }
-
 //メインループ
 bool Field::MainLoop(){
 	FallObjects();
@@ -18,44 +14,163 @@ bool Field::MainLoop(){
 	TouchPlayer2Objects();
 	TouchObjects2Wall();
 	DeleteObjects();
+	CalcOffset();
 	DrawObjects();
 
 }
 
 void Field::Scroll(){
-
 }
 
+//完成
 void Field::FallObjects(){
-	for(int i=0; i<objects.size;i++)
-		objects.size;
+	for(int i=0; i<objects_.size(); i++)
+		objects_.at(i)->Fall(gravity_);
 }
 
+//完成
 void Field::MoveObjects(){
+	for(int i=0; i<objects_.size(); i++)
+		objects_.at(i)->Move();
 }
 
+//未完成(マップ全体の長さがわからない)
 void Field::DrawObjects(){
+	//描画
+	for(int i=0; i<objects_.size(); i++)
+		objects_.at(i)->Draw(offset);
 }
 
+//完成
 void Field::ThinkObjects(){
+	for(int i=0; i<objects_.size(); i++)
+		objects_.at(i)->Think();
 }
 
+//未完成（当たり判定は円で行う,キャラの大きさは32で統一して行っている）
 void Field::TouchPlayer2Objects(){
+		int px = (int)objects_.at(0)->pos().x;
+		int py = (int)objects_.at(0)->pos().y;
+	//objects_の要素０はプレイヤーなのでi=1からはじめる
+	for(int i=1; i<objects_.size(); i++){
+		//プレイヤーと敵との接触判定
+		if(JudgeCircle(px, py, 16, (int)objects_.at(i)->pos().x, (int)objects_.at(i)->pos().y, 16)){
+
+		}
+		//プレイヤーの弾と敵とのあたり判定
+
+		//敵の弾とプレイヤーとのあたり判定
+
+	}
 }
 
+//完成（仮）
 void Field::TouchObjects2Wall(){
+	for(int i=0; i<objects_.size(); i++){
+		double objectX = objects_.at(i)->pos().x;
+		double objectY = objects_.at(i)->pos().y;
+		double newX;
+		double newY;
+		//x方向とy方向に分けて判定を行うので
+		//k=0の時x方向,k=1の時y方向
+		for(int k=0; k<2; k++){
+			if(k==0){
+				newX = objectX+objects_.at(i)->speed().x;
+				newY = objectY;
+			}
+			else{
+				newX = objectX;
+				newY = objectY+objects_.at(i)->speed().y;
+			}
+			newX = floor(newX);
+			newY = floor(newY);
+	
+			double fromX = min(objectX,newX);
+			double fromY = min(objectY,newY);
+			double toX = max(objectX,newX);
+			double toY = max(objectY,newY);
+
+			int fromTileX = pixelsToTiles(fromX);
+			int fromTileY = pixelsToTiles(fromY);
+			int toTileX = pixelsToTiles(toX+31);
+			int toTileY = pixelsToTiles(toY+31);
+
+			//衝突しているか調べる
+			for(int x = fromTileX; x<= toTileX; x++){
+				for(int y = fromTileY; y <= toTileY; y++){
+					if(map_.GetMapData(x,y) == 1){
+						if(k==0){  //x方向であたっていた場合
+							objects_.at(i)->TouchedBlockX(TilesToPixels(x));	
+						} 
+						if(k==1){ //y方向であたっていた場合
+							objects_.at(i)->TouchedBlockY(TilesToPixels(y));
+						} 
+					}
+				}
+			}
+		}
+	}
 }
 
+//完成（仮）
 void Field::DeleteObjects(){
+	for(int i=0; i<objects_.size(); i++){
+		if(!objects_.at(i)->alive()){
+			//オブジェクト削除
+			delete objects_.at(i);
+			//オブジェクトの参照削除
+			vector<Chara*>::iterator start;
+			start = v.begin()+i;
+			v.erase(start);
+		}
+	}
 }
 
+//完成（仮）
 void Field::CheckOutOfArea(){
+	for(int i=0; i<objects_.size(); i++){
+		if(objects_.at(i)->pos().x < offset || 
+			objects_.at(i)->pos().x > offset+480 ){
+			//オブジェクト削除
+			delete objects_.at(i);
+			//オブジェクトの参照削除
+			vector<Chara*>::iterator start;
+			start = v.begin()+i;
+			v.erase(start);
+		}
+	}
 }
 
-void Field::AddObject(int object_num){
+//未完成(マップからの情報)
+void Field::AddObject(AObject *object_num){
+	objects_.push_back(object_num);
+	vector<int>::iterator start;
+	start = test.begin()+2;
+	test.erase(start);
 }
 
+void Field::CalcOffset(){
+	int width = 480;  //画面の横幅
+	offset = width/2-(int)objects_.at(0)->pos().x;
+	offset = min(offsetX,0);
+	offset = max(offsetX,width-100);
+}
 
+int pixelsToTiles(double pixels){
+	return floor(pixels/32);
+}
 
+int tilesToPixels(int tiles){
+	return tiles*32;
+}
 
+bool JudgeCircle(int x1, int y1, int r1, int x2, int y2, int r2){
+		int x = (x1-x2)*(x1-x2);
+		int y = (y1-y2)*(y1-y2);
+		int l = x+y;
+		int r = (r1+r2)*(r1+r2);
+		if(l<r)
+			return true;
+		return false;
+}
 
--- Field.h
+++ Field.h
@@ -1,33 +1,46 @@
 # ifndef FIELD_H
 # define FIELD_H
 #pragma once
+#include "Map.h"
+#include"AObject.h"
+#include "Character.h"
+#include <math.h>
+#include <vector>
 using namespace std;
 
 class Field
 {
 //フィールド
 private:
-	Map map;                    //マップ
-	int count;                  //時間計測用
-	vector<int> objects;    //全てのオブジェクト管理用
-	double gravity;             //重力の値
-
+	Map map_;                    //マップ
+	int count_;                  //時間計測用
+	vector<AObject*> objects_;    //全てのオブジェクト管理用
+	vector<Character*> charas_;
+	double gravity_;             //重力の値
+	int offset;                  //オフセット
+	
 //メソッド
 public:
-	Field(void);      //コンストラクタ
+	Field(Map map);   //コンストラクタ
 	bool MainLoop();  //メインループ
+	
 
 private:
-	void Scroll();                  //スクロール処理
-	void FallObjects();             //各オブジェクトに対して、空中にいる場合Fallを呼び出す
-	void MoveObjects();             //各オブジェクトに対してMoveを呼び出す
-	void DrawObjects();             //各オブジェクトに対してDrawを呼び出す
-	void ThinkObjects();            //各オブジェクトに対して
-	void TouchPlayer2Objects();     //プレイヤー、弾と敵、アイテムとの当たり判定
-	void TouchObjects2Wall();       //オブジェクトと壁の当たり判定
-	void DeleteObjects();           //生存falgがfalseのオブジェクトを削除
-	void CheckOutOfArea();          //画面外にオブジェクトがあれば削除 
-	void AddObject(int object_num); //引数のオブジェクトを生成
+	void Scroll();                       //スクロール処理
+	void FallObjects();                  //各オブジェクトに対して、空中にいる場合Fallを呼び出す
+	void MoveObjects();                  //各オブジェクトに対してMoveを呼び出す
+	void DrawObjects();                  //各オブジェクトに対してDrawを呼び出す
+	void ThinkObjects();                 //各オブジェクトに対して
+	void TouchPlayer2Objects();          //プレイヤー、弾と敵、アイテムとの当たり判定
+	void TouchObjects2Wall();            //オブジェクトと壁の当たり判定
+	void DeleteObjects();                //生存falgがfalseのオブジェクトを削除
+	void CheckOutOfArea();               //画面外にオブジェクトがあれば削除 
+	void AddObject(AObject *object_num); //引数のオブジェクトを生成
+	int count(){return count_;}          //countのゲッター
+	void CalcOffset();                   //offsetを計算
+	int PixelToTiles(double pixels);     //ピクセル単位をタイル単位に変換
+	int TilesToPixels(int tiles);        //タイル単位をピクセル単位に変換
+	bool JudgeCircle(int x1, int y1, int r1, int x2, int y2, int r2) //円同士のの当たり判定
 	
 };
 #endif
