68effa07706c1a33cfecfa209687b60e7706e67b
222yuta <info@info-no-MacBook-Air-48.local>
2014-08-23 19:33:34 +0900

    NormalBulletの修正
    反対方向に発射されるバグを修正

--- NormalAttack.cpp
+++ NormalAttack.cpp
@@ -11,7 +11,7 @@ NormalAttack::NormalAttack(int damage, int speed, int interval, Character* chara
 }
 
 void NormalAttack::DoAttack(){
-	Attack::DoAttack(new NormalBullet(chara_->GetCenterPosX(), chara_->GetCenterPosY(), damage_, speed_, 1,1,true));
+	Attack::DoAttack(new NormalBullet(chara_->GetCenterPosX(), chara_->GetCenterPosY(), damage_, speed_, 1,1,chara_->isRight()));
 }
 
 void NormalAttack::InitializeBullet(int num){
